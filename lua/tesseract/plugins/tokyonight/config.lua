25c8fdc033112b9059c0cfc68a73a34884973d46
Mauro Kinderknecht <mauro.mkinderknecht@gmail.com>
2023-01-07 18:14:26 -0300

    override colors

--- lua/tesseract/plugins/tokyonight/config.lua
+++ lua/tesseract/plugins/tokyonight/config.lua
@@ -8,6 +8,15 @@ local config = u.merge({
   light_style = 'moon',
   transparent = true,
   sidebars = { 'qf', 'packer', 'help' },
+  on_colors = function(colors)
+    colors.comment = '#bbc0d4'
+  end,
+  on_highlights = function(hl, c)
+    hl.LineNr = {
+      fg = '#606b9e'
+    }
+
+  end,
 }, user_config.plugins.tokyonight or {})
 
 return config
