afdd490c0cc29eb0f6f254d4e9680d8347932c99
Mauro Kinderknecht <mauro.mkinderknecht@gmail.com>
2023-01-07 17:11:30 -0300

    added template config files

---
+++ lua/tesseract/config/config.template.lua
@@ -0,0 +1,28 @@
+-- Override configuration options
+
+local config = {
+    -- See :h nvim_open_win for possible border options
+    border = 'rounded',
+
+    -- LSP settings
+    lsp = {
+      
+    },
+  
+    -- adjust default plugin settings
+    plugins = {
+      
+    },
+  
+    -- Disable plugins default enabled by CosmicNvim
+    disable_builtin_plugins = {
+      
+    },
+  
+    -- Add additional plugins (lazy.nvim)
+    add_plugins = {
+      
+    },
+}
+
+return config
---
+++ lua/tesseract/config/editor.template.lua
@@ -0,0 +1,5 @@
+-- Override editor options
+
+local g = vim.g
+local map = require('tesseract.utils').map
+local opt = vim.opt
